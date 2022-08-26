eaf1cfa46bc5b433219056ad5adc484a353beecc
apple <apple@192.168.0.102>
2022-08-26 16:55:14 +0800

    项目基本组件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
 
     <router-view />
     <h1>ashd</h1>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+    <div>
+      <i class="toutiao toutiao-fenxiang"></i>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import router from './router'
 import store from './store'
 // 加载全局
 import './styles/index.less'
+// 加载vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 加载rem适配
+import 'amfe-flexible'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
